2262fa588aa4ff2f810eb210bd492c65d3d965b9
Andre Espinoza <ae95caba@gmail.com>
2022-11-17 19:43:04 -0300

    missing only modules

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const gameboardHtml = document.getElementById("gameboard");
+gameboardHtml.style.display = "none"
+
+
+let winnerDetectedSwitch; 
 let gameboard = ["","","","","","","","",""];
 let Gameboard = {gameboard};
 let game={playerTurn: "x", SwitchTurn: function(){
@@ -25,6 +30,7 @@ function displayAR(){
 }
 
 displayAR();
+
 function mark(e){
     //console.log(e.target.innerText);
     if(e.target.innerText){return}
@@ -45,24 +51,37 @@ function addMarkToBoard(){
     }
 }
 
+function removeMarkToBoard(){
+    for(let i = 0 ; i<9; i++){
+
+        document.getElementById(i).removeEventListener("click",mark);
+    }
+}
+
+
 addMarkToBoard();
 
 ////////////////////////////////////////////////
 
 function detectWin(player){
-if(
-gameboard[0] == player && gameboard[1] == player && gameboard[2] == player ||
-gameboard[3] == player && gameboard[4] == player && gameboard[5] == player ||
-gameboard[6] == player && gameboard[7] == player && gameboard[8] == player ||
-///////////////////////////////////////////////////
-gameboard[0] == player && gameboard[3] == player && gameboard[6] == player ||
-gameboard[1] == player && gameboard[4] == player && gameboard[7] == player ||
-gameboard[2] == player && gameboard[5] == player && gameboard[8] == player ||
-///////////////////////////////////////////////////
-gameboard[0] == player && gameboard[4] == player && gameboard[8] == player ||
-gameboard[2] == player && gameboard[4] == player && gameboard[6] == player 
-){alert(`${player} won the game`);};
-}
+    if(
+    gameboard[0] == player && gameboard[1] == player && gameboard[2] == player ||
+    gameboard[3] == player && gameboard[4] == player && gameboard[5] == player ||
+    gameboard[6] == player && gameboard[7] == player && gameboard[8] == player ||
+    ///////////////////////////////////////////////////
+    gameboard[0] == player && gameboard[3] == player && gameboard[6] == player ||
+    gameboard[1] == player && gameboard[4] == player && gameboard[7] == player ||
+    gameboard[2] == player && gameboard[5] == player && gameboard[8] == player ||
+    ///////////////////////////////////////////////////
+    gameboard[0] == player && gameboard[4] == player && gameboard[8] == player ||
+    gameboard[2] == player && gameboard[4] == player && gameboard[6] == player 
+    ){
+        display.innerText = `${player} won the game`;
+        removeMarkToBoard();
+        winnerDetectedSwitch = true;
+        }
+    };
+
 
 function detectTie(){
     let cellsFull = 0
@@ -70,7 +89,9 @@ function detectTie(){
         if(gameboard[i]){cellsFull++;};
     }
     if (cellsFull == 9) {
-        alert("is a tie");
+        
+        display.innerText="is a tie";
+         
     }
 }
 
@@ -81,6 +102,7 @@ const form = document.getElementById("myForm");
 const player1Name = document.getElementById("player-one");
 const player2Name = document.getElementById("player-two");
 const playerSelection = document.getElementById("player-selection");
+const startButton = document.getElementById("start");
 
 form.addEventListener("submit",function(e){
     e.preventDefault();
@@ -93,6 +115,26 @@ form.addEventListener("submit",fun1);
 function fun1 (){
     player1 = PlayerFactory(player1Name.value, "x");
     player2 = PlayerFactory(player2Name.value,"o");
+    player1Name.value="";
+    player2Name.value="";
     playerSelection.style.display = "none" 
+    gameboardHtml.style.display = "grid";
+    startButton.style.display = "none";
+
 
 }
+
+//////////////////////////////////////////////////
+//reset and display
+const display = document.getElementById("display");
+const reset = document.getElementById("reset");
+reset.addEventListener("click",fun2);
+
+function fun2(){
+    gameboard = ["","","","","","","","",""];
+    displayAR();
+    if(winnerDetectedSwitch){addMarkToBoard();};
+    display.innerText = "";
+    //playerSelection.style.display = "block";
+}
+
